5c43e39a3bcf9dbe117398366a48f99e8eae5526
Matt Larson <matt.a.larson@gmail.com>
2017-10-04 17:24:11 -0500

    changed pallete and played around throughout app

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -19,8 +19,8 @@ export class ListPage {
     this.items = [];
     for(let i = 1; i < 11; i++) {
       this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
+        title: 'Spursy Item ' + i,
+        note: 'This is Spursy item #' + i,
         icon: this.icons[Math.floor(Math.random() * this.icons.length)]
       });
     }
